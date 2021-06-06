01762939366ed5cedd6319715fed3328949d78a4
Jamshid <mamatovjamshid506@gmail.com>
2021-06-06 11:49:52 +0500

    write in response

--- mypro/mypro/urls.py
+++ mypro/mypro/urls.py
@@ -3,8 +3,10 @@ from django.urls import path
 from django.http import HttpRequest,HttpResponse
 
 def home(request):
-
-    return HttpResponse("ok")
+    response=HttpResponse()
+    response.write("<h1>hello django</h1>")
+    response.write("<p>start project</p>")
+    return response
 
 
 urlpatterns = [
