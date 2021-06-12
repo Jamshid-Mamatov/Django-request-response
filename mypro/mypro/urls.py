d36fc1ecbb1ff987a7ee85005f110eda3e39a4fd
Jamshid <mamatovjamshid506@gmail.com>
2021-06-12 15:19:42 +0500

    add app

--- mypro/mypro/urls.py
+++ mypro/mypro/urls.py
@@ -1,34 +1,9 @@
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpRequest,HttpResponse
-
-def home(request):
-    response=HttpResponse()
-    # response.write("<h1>hello django</h1>")
-    # response.write("<p>start project</p>")
-
-
-    # # print(response)
-    # # print(type(response))
-    # response.__setitem__('age',19)          # item set in header  
-    # response.__setitem__('name','jamshid')
-
-    # print(response.items())                 # information of headers
-
-    # response.__delitem__("name")            # item of header del
-
-    
-    # print(response.items())
-
-    
-    a=request.GET.get("a")
-    b=request.GET.get("b")
-    sum=int(a)+int(b)
-    response.write(f"<h1>Sum: {sum} </h1>")
-    return response
+from django.urls import path,include
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',home)
+    path('',include('myapp.urls'))
+    
 ]
